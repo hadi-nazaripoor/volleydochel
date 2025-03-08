5bef9e57e17ccf77eed0f94d3f436770067bb686
Hadi <Hadi@DESKTOP-H87E9EJ>
2025-03-08 23:44:43 +0330

    remove unused components' refrences.

--- components/stats-grid.tsx
+++ components/stats-grid.tsx
@@ -1,4 +1,4 @@
-import { RiArrowRightUpLine, RiArrowRightDownLine } from "@remixicon/react";
+import { RiArrowRightUpLine } from "@remixicon/react";
 import { cn } from "@/lib/utils";
 
 interface StatsCardProps {
